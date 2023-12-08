a6f600bcdef247b79f47f4cd1eb69f6dabdee1f0
Jonathan Vargas <jvargas@linkdigital.es>
2023-12-07 21:53:31 -0600

    fix deployment on vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
-import { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth/next";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
